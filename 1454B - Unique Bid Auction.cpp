c7b059252b5d3d302b493bc554b62ad777228192
Sudip-329 <sudipchakrabarty6@gmail.com>
2026-02-17 15:59:46 +0600

    Update 1454B - Unique Bid Auction.cpp

--- 1454B - Unique Bid Auction.cpp
+++ 1454B - Unique Bid Auction.cpp
@@ -4,14 +4,11 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
     int t;
     cin >> t;
-
     while (t--) {
         int n;
         cin >> n;
-
         vector<int> a(n);
         vector<int> freq(n + 1, 0);
         vector<int> index(n + 1, -1);
