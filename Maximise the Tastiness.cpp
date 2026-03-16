133c8b6b5d144d7f921ae76271035847ab80e3c1
Sudip-329 <sudipchakrabarty6@gmail.com>
2026-03-17 01:12:41 +0600

    Create Maximise the Tastiness.cpp

---
+++ Maximise the Tastiness.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        cout << max(a, b) + max(c, d) << endl;
+    }
+
+    return 0;
+}
