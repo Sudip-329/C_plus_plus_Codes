f62dbc063c2ecf6882570b9ed6be31a3c9dfa323
Sudip <sudipchakrabarty6@gmail.com>
2026-03-31 15:13:12 +0600

    Remove comments and whitespace in main function

--- Recurrence Reachability.cpp
+++ Recurrence Reachability.cpp
@@ -34,15 +34,12 @@ void solve() {
 }
 
 int main() {
-    // Fast I/O
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
     int t;
     cin >> t;
     while (t--) {
         solve();
     }
-    
     return 0;
 }
