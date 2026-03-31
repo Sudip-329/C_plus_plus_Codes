63b30eef8e6d8d2ec0903d7f8abfae07ffa41482
Sudip <sudipchakrabarty6@gmail.com>
2026-03-31 14:54:58 +0600

    Clean up comments in Imperfect Numbers.cpp
    Removed unnecessary comments to improve code clarity.

--- Imperfect Numbers.cpp
+++ Imperfect Numbers.cpp
@@ -10,18 +10,15 @@ void solve() {
     if ((n % 2 == 0 || n % 5 == 0) && (n % 10 != 0)) {
         cout << 0 << "\n";
     } 
-    // Check if the number is a multiple of 10
     else if (n % 10 == 0) {
         cout << 2 << "\n";
     } 
-    // Number is not divisible by 2 or 5 (ends in 1, 3, 7, 9)
     else {
         cout << 1 << "\n";
     }
 }
 
 int main() {
-    // Optimize standard I/O operations for faster execution
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
