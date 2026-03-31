14799af2d67cd04cd3682df184106ee23a8e8f2f
Sudip <sudipchakrabarty6@gmail.com>
2026-03-31 14:52:41 +0600

    Update Snaky Strings.cpp

--- Snaky Strings.cpp
+++ Snaky Strings.cpp
@@ -4,13 +4,10 @@
 using namespace std;
 
 int main() {
-    // Fast I/O
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
     string A;
     if (cin >> A) {
-        // Check if the string starts or ends with 's'
         if (A.front() == 's' || A.back() == 's') {
             cout << "Yes\n";
         } else {
